e300498f1709abc4e760daa8381833debeeee58c
atknklca <47725162+atknklca@users.noreply.github.com>
2021-05-02 02:40:36 +0300

    ChangeName Completed

--- app/src/main/java/com/example/expensetracker/ui/fragment/ChangeNameFragment.kt
+++ app/src/main/java/com/example/expensetracker/ui/fragment/ChangeNameFragment.kt
@@ -1,19 +1,59 @@
 package com.example.expensetracker.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.expensetracker.R
+import com.example.expensetracker.databinding.FragmentChangeNameBinding
 
 class ChangeNameFragment : Fragment() {
+
+    private lateinit var binding: FragmentChangeNameBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_name, container, false)
+        binding = FragmentChangeNameBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var gender : Int? = null
+
+        binding.radioGroup.setOnCheckedChangeListener { radioGroup, id ->
+            gender = when(id){
+                R.id.radioButtonErkek -> { 1 }
+                R.id.radioButtonKadin -> { 2 }
+                else -> { 3 }
+            }
+        }
+
+        binding.buttonSaveName.setOnClickListener {
+            if (binding.inputName.editText?.text.toString().isNotEmpty() && binding.radioGroup.checkedRadioButtonId != -1){
+
+                val name = binding.inputName.editText?.text.toString()
+                val sharedPref = requireActivity().getSharedPreferences("Name", Context.MODE_PRIVATE)
+
+                val editor = sharedPref.edit()
+                editor.putString("Name", name)
+                editor.putInt("gender",gender!!)
+                editor.apply()
+
+                findNavController().navigate(R.id.action_changeNameFragment2_to_homePageFragment)
+            }else{
+                Toast.makeText(requireContext(),"Lütfen Boş Alan Bırakmayınız!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/expensetracker/ui/fragment/HomePageFragment.kt
+++ app/src/main/java/com/example/expensetracker/ui/fragment/HomePageFragment.kt
@@ -1,19 +1,42 @@
 package com.example.expensetracker.ui.fragment
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.ContextMenu
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.expensetracker.R
+import com.example.expensetracker.databinding.FragmentFirstScreenBinding
+import com.example.expensetracker.databinding.FragmentHomePageBinding
 
 class HomePageFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomePageBinding
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false)
+        binding = FragmentHomePageBinding.inflate(inflater,container, false)
+        val view = binding.root
+        return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedPref = requireActivity().getSharedPreferences("Name", Context.MODE_PRIVATE)
+
+    binding.layoutMerhaba.setOnClickListener{
+        findNavController().navigate(R.id.action_homePageFragment_to_changeNameFragment2)
+    }
+    }
+
+
 }
